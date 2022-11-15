eb8df133c28c92b5be07e06e1ef94c8a78d41a59
CallMeJohny <taffo563@gmail.com>
2022-11-15 21:01:35 +0100

    detonation

---
+++ detonation.py
@@ -0,0 +1,9 @@
+from time import sleep
+
+x = 10
+while x > 0:
+    print(f"Detonating in {x}")
+    x -= 1
+    sleep(1)
+print("Detonated succesfully")
+print(f"{x} je super a jeste lepsi je {y}")
